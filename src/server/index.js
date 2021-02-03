1cb1ea90f26aee6ef2132fd312c728bd4e9a01ef
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-02 23:59:22 -0600

    added scss files

--- src/server/index.js
+++ src/server/index.js
@@ -6,11 +6,7 @@ const app = express()
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
-
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    //res.sendFile(path.resolve('src/client/views/index.html'))
     res.sendFile('dist/index.html')
 })
 
