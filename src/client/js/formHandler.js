4c8c84050ece5773da092005eec56657b44ed36f
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-03 23:09:20 -0600

    added api info and finshed adding function fixes

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,12 +3,18 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8080/process-text', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ test: "test" })
+    })
     .then(res => res.json())
     .then(function(res) {
+        console.log(res)
+        console.log('test')
         document.getElementById('results').innerHTML = res.message
     })
 }
