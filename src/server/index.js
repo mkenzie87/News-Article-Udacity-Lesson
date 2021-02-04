aaa666983c1bef11352b789a6c1939a87ea82963
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-03 20:17:28 -0600

    started working with meaning api

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,25 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+// let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=c0a2c728bb89503cdb9f258ba866df9b&lang=en&txt=hello&model=general';
+// const apiKey = '13d2bf320cf4836f44b05ff1af01636a';
+
+
+
+app.post('/process-text', async function(req, res) {
+
+  fetch("https://api.meaningcloud.com/sentiment-2.1?key=13d2bf320cf4836f44b05ff1af01636a&lang=en&txt=hello&model=general", {
+    method: "GET"
+  }).then((response) => {
+    return response.json();
+  }).then((data) => {
+      res.send(data)
+      console.log(data);
+  })
+
+
+})
+
 app.use(express.static('dist'))
 
 app.get('/', function (req, res) {
