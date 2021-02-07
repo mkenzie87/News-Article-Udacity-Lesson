11da87214fe7075bcaa47cab5272de010826054e
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-02-07 16:45:07 -0600

    hiding the env file

--- src/server/index.js
+++ src/server/index.js
@@ -4,38 +4,63 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.use(express.static('dist'))
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
+app.listen(8080, function() {
   console.log('Example app listening on port 8080!')
 })
 
-// let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=c0a2c728bb89503cdb9f258ba866df9b&lang=en&txt=hello&model=general';
-// const apiKey = '13d2bf320cf4836f44b05ff1af01636a';
 
-app.post('/process-text', function (req, res) {
-  console.log("hit")
-  fetch("https://api.meaningcloud.com/sentiment-2.1?key=13d2bf320cf4836f44b05ff1af01636a&lang=en&txt=hello&model=general", {
-    method: "GET"
+app.get('/', function(req, res) {
+  res.sendFile('dist/index.html')
+})
+
+app.get('/test', function(req, res) {
+  res.send(mockAPIResponse)
+})
+
+
+/
+
+
+app.post('/process-text', function(req, res) {
+
+  // meaningcloud api structure
+  // let baseURL = 'https://api.meaningcloud.com/sentiment-2.1?key=APIKEYHERE&lang=en&txt=hello&model=general';
+
+
+  // Establishing API Info
+  const apiKey = process.env.API_KEY; // meaningcloud API key hidden
+
+  // Building Fetch URL Using Base Url then adding Params with API Key and URL Info
+  const baseURL = 'https://api.meaningcloud.com/sentiment-2.1' //meaningcloud base request url
+  const allParams = `?key=${apiKey}&lang=en&model=general` // adding API Params
+  const fetchURL = baseURL + allParams; // builing full Fetch Url
+
+  console.log(fetchURL); // logging Fetch URL
+
+
+
+  fetch("fetchURL", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
   }).then((response) => {
     return response.json();
   }).then((data) => {
     console.log(data);
     res.send(data)
   })
-})
-
-app.get('/', function (req, res) {
-  res.sendFile('dist/index.html')
-})
-
-app.get('/test', function (req, res) {
-  res.send(mockAPIResponse)
 })
